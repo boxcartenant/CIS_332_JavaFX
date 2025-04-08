836fd452bcdb230259e0bc78d5f3925c8795691d
boxcartenant <zacjslade@gmail.com>
2025-04-08 19:34:05 -0400

    a commitment

--- src/main/java/my_new_package/MainApp.java
+++ src/main/java/my_new_package/MainApp.java
@@ -1,8 +1,29 @@
 package my_new_package;
 
-public class MainApp {
+import javafx.application.Application;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+
+    // The stage is the main window of the javafx application
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("SceneOne.fxml"));
+        Scene scene1 = new Scene(fxmlLoader.load(), 600, 400);
+
+        primaryStage.setTitle("Scene 1 - Welcome");
+        primaryStage.setScene(scene1);
+
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
+        //Singleton stuff below here:
         SingletonDemo singleton = SingletonDemo.getInstance();
 
         singleton.showMessage();
@@ -10,5 +31,7 @@ public class MainApp {
         SingletonDemo anotherSingleton = SingletonDemo.getInstance();
 
         System.out.println("Same instance? " + (singleton == anotherSingleton));
+
+        launch();
     }
 }
